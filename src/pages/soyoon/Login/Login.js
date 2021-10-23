dbac69b1667f19024cbd603a37e8fd782c1ac1ef
soyoon <eenooyos@gmail.com>
2021-10-23 19:42:49 +0900

    Add : 개인페이지 구현 완료.

--- src/pages/soyoon/Login/Login.js
+++ src/pages/soyoon/Login/Login.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import './Login.scss';
 
 class LoginSoyoon extends React.Component {
+  goMain = () => {
+    this.props.history.push('/main-soyoon');
+  };
+
   render() {
     return (
-      <div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <h1>soyoon 로그인페이지</h1>
+      <div className="Login">
+        <section className="login-name">
+          <h1>WESTAGRAM</h1>
+        </section>
+        <div className="login-wrapper">
+          <div className="login-input">
+            <input
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+              id="login-email"
+            />
+            <input type="password" placeholder="비밀번호" id="login-pw" />
+          </div>
+          <button onClick={this.goMain} type="submit" id="login-btn">
+            로그인
+          </button>
+        </div>
+        <div className="login-text">
+          <p>
+            <a href="#">비밀번호를 잊으셨나요?</a>
+          </p>
+        </div>
       </div>
     );
   }
--- src/pages/soyoon/Main/Main.js
+++ src/pages/soyoon/Main/Main.js
@@ -1,11 +1,219 @@
 import React from 'react';
+import './Main.scss';
 
 class MainSoyoon extends React.Component {
   render() {
     return (
-      <div>
-        <h1>soyoon Mainpage</h1>
-      </div>
+      <main>
+        <article className="article">
+          <section className="feeds">
+            <div className="feeds-head">
+              <div className="feeds-head-left">
+                <div className="feeds-head-img"></div>
+                <div className="feeds-head-name">
+                  <p>eenooyos</p>
+                </div>
+              </div>
+              <div className="feeds-head-right">
+                <img src="https://cdn1.iconfinder.com/data/icons/jumpicon-basic-ui-glyph-1/32/-_Dot-More-Option-Menu-1024.png" />
+              </div>
+            </div>
+            <div className="feeds-img"></div>
+            <div className="feeds-img-int">
+              <div className="feeds-img-nav">
+                <div className="img-nav-wrapper">
+                  <div className="heart">
+                    <i className="far fa-heart"></i>
+                  </div>
+                  <div className="bubble">
+                    <i className="far fa-comment"></i>
+                  </div>
+                  <div>
+                    <i className="far fa-paper-plane"></i>
+                  </div>
+                </div>
+                <div>
+                  <div className="img-nav-wrapper">
+                    <i className="far fa-bookmark"></i>
+                  </div>
+                </div>
+              </div>
+              <div className="feeds-img-re">
+                <div className="feeds-img-like">
+                  <div className="like-img"></div>
+                  <div className="like-img-ex">
+                    <p>
+                      <a href="#">S2minjeong</a>님 <a href="#">외 23명</a>이
+                      좋아합니다.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="feeds-commit">
+                  <p>
+                    <a href="#" class="name">
+                      eenooyos
+                    </a>{' '}
+                    아이고 두야.. 이렇게 하는거 맞는거... <span>더보기</span>
+                  </p>
+
+                  <ul id="comment-wrapper">
+                    <li>
+                      <span className="name">yeonjeongzzang</span>
+                      <span> 마라탕조아용😏</span>
+                      <span class="delete"> x </span>
+                    </li>
+                  </ul>
+
+                  <p>42분 전</p>
+                </div>
+              </div>
+              <div id="comment">
+                <input
+                  type="text"
+                  placeholder="댓글 달기..."
+                  id="commentInput"
+                />
+                <button id="submit">게시</button>
+              </div>
+            </div>
+          </section>
+
+          <section id="side">
+            <div className="side-account">
+              <div className="account-img"></div>
+              <div className="account-img-ex">
+                <div>
+                  <p>wecode_bootcamp</p>
+                  <p>
+                    WeCode<span>위코드</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="side-story">
+              <div className="side-navi">
+                <p>스토리</p>
+                <div>모두 보기</div>
+              </div>
+              <div className="story">
+                <div className="story-img">
+                  <div></div>
+                </div>
+                <div className="story-img-ex">
+                  <p>sanghyunboy</p>
+                  <p>16분 전</p>
+                </div>
+              </div>
+              <div className="story">
+                <div className="story-img">
+                  <div></div>
+                </div>
+                <div className="story-img-ex">
+                  <p>you_god</p>
+                  <p>20분 전</p>
+                </div>
+              </div>
+              <div className="story">
+                <div className="story-img">
+                  <div></div>
+                </div>
+                <div className="story-img-ex">
+                  <p>iamwindow</p>
+                  <p>1시간 전</p>
+                </div>
+              </div>
+
+              <div className="story">
+                <div className="story-img">
+                  <div></div>
+                </div>
+                <div className="story-img-ex">
+                  <p>sanghyunboy</p>
+                  <p>3시간 전</p>
+                </div>
+              </div>
+              <div className="story">
+                <div className="story-img">
+                  <div></div>
+                </div>
+                <div className="story-img-ex">
+                  <p>you_god</p>
+                  <p>5시간 전</p>
+                </div>
+              </div>
+              <div className="story">
+                <div className="story-img">
+                  <div></div>
+                </div>
+                <div className="story-img-ex">
+                  <p>iamwindow</p>
+                  <p>20시간 전</p>
+                </div>
+              </div>
+            </div>
+            <div className="side-pick">
+              <div className="side-navi">
+                <p>회원님을 위한 추천</p>
+                <div>모두 보기</div>
+              </div>
+              <div className="suggestion">
+                <div className="suggestion-left">
+                  <div className="suggestion-left-img"></div>
+                  <div className="left-img-ex">
+                    <p>sunghomayo</p>
+                    <p>you_god님 외 2명이 팔...</p>
+                  </div>
+                </div>
+                <button>팔로우</button>
+              </div>
+              <div className="suggestion">
+                <div className="suggestion-left">
+                  <div className="suggestion-left-img"></div>
+                  <div className="left-img-ex">
+                    <p>zeehoojoah</p>
+                    <p>yeonjeongzzang님 외...</p>
+                  </div>
+                </div>
+                <button>팔로우</button>
+              </div>
+              <div className="suggestion">
+                <div className="suggestion-left">
+                  <div className="suggestion-left-img"></div>
+                  <div className="left-img-ex">
+                    <p>boradori_</p>
+                    <p>iamwindow님 외 1명이...</p>
+                  </div>
+                </div>
+                <button>팔로우</button>
+              </div>
+            </div>
+
+            <footer className="footer">
+              <ul>
+                <li>Instagram 정보</li>
+                <li>지원</li>
+                <li>홍보</li>
+                <li>지원</li>
+                <li>홍보센터</li>
+                <li>API</li>
+              </ul>
+              <ul>
+                <li>채용정보</li>
+                <li>개인정보처리방침</li>
+                <li>약관</li>
+              </ul>
+              <ul>
+                <li>디렉터리</li>
+                <li>프로필</li>
+                <li>해시태그</li>
+                <li>언어</li>
+              </ul>
+              <p>ⓒ 2019 INSTAGRAM</p>
+            </footer>
+          </section>
+        </article>
+      </main>
     );
   }
 }

0709e9753fbf9b28e1232a85450d1eafb1963585
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2021-10-23 19:44:13 +0900

    add : 개인페이지 구현 완료

--- src/pages/jaewoo/Login/Login.js
+++ src/pages/jaewoo/Login/Login.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.scss';
 
-class LoginJaewoo extends React.Component {
+class Login extends React.Component {
   render() {
     return (
-      <div>
-        <h1>ㅈㅐ우님 로그인페이지</h1>
-      </div>
+      <body>
+        <div class="bigbox">
+          <div class="logoTop"> westagram</div>
+
+          <div class="login">
+            <div>
+              <input class="id" placeholder="전화번호,사용자이름 또는 이메일" />
+            </div>
+            <div>
+              <input class="pwd" placeholder="비밀번호" />
+            </div>
+            <div>
+              <Link to="/Main">
+                <button class="btn">로그인</button>
+              </Link>
+            </div>
+          </div>
+          <div class="forget">
+            <span class="lostPassword">비밀번호를 잊으셨나요?</span>
+          </div>
+        </div>
+      </body>
     );
   }
 }
-
-export default LoginJaewoo;
+export default Login;

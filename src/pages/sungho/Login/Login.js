8140a473e7d21e360435449030697b559f09b474
양성호 <sungho@yangseonghoui-MacBookAir.local>
2021-10-25 21:13:06 +0900

    Add: 미션 1번 완료

--- src/pages/sungho/Login/Login.js
+++ src/pages/sungho/Login/Login.js
@@ -2,17 +2,43 @@ import React from 'react';
 import './Login.scss';
 
 class LoginSungho extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      idValue: '',
+      pwValue: '',
+    };
+  }
+
+  handleIdInput = event => {
+    this.setState({
+      idValue: event.target.value,
+    });
+  };
+
+  handlePwInput = event => {
+    this.setState({
+      pwValue: event.target.value,
+    });
+  };
+
   render() {
     return (
       <div class="login-page">
         <h1>Westagram</h1>
         <form action="" id="loginForm">
           <input
+            onChange={this.handleIdInput}
             class="id"
             type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
           />
-          <input class="pw" type="password" placeholder="비밀번호" />
+          <input
+            onChange={this.handlePwInput}
+            class="pw"
+            type="password"
+            placeholder="비밀번호"
+          />
           <button class="button" type="button" disabled>
             로그인{' '}
           </button>
